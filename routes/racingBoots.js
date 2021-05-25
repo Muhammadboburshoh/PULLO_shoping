f0a00a8096c1e04cb21db366ea748f062444edd0
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-25 22:54:30 +0500

    Finished page

--- routes/racingBoots.js
+++ routes/racingBoots.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 const { row } = require("../util/pg")
 
+const bestProductSQL = `
+  select * from products where product_new = true and product_rating > 5
+`
+
 router.get('/', async function(req, res, next) {
 
   const bestProduct = await row(bestProductSQL)
 
-  res.render('racingBoots');
+  res.render('racingBoots', {bestProduct});
 });
 
 module.exports = router;
