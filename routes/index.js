fdbf3e04065d0106c6ff4c9f0427aae68ce01dff
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-25 20:14:40 +0500

    connection home page backend

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ var router = express.Router();
 
 const { row, rows } = require("../util/pg")
 
+const introSQL = `
+  select * from products where product_new = true and product_rating = 5
+`
+
+const bestProductSQL = `
+  select * from products where product_new = true and product_rating > 5
+`
+
+const productsSQL = `
+  select * from products where product_new = false and product_rating = 5
+`
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-  const product = await rows('select * from products')
-  console.log(product);
-  res.render('index', { title: 'Express' });
+  const introProduct = await rows(introSQL)
+  const bestProduct = await row(bestProductSQL)
+  const products = await rows(productsSQL)
+  console.log(products);
+
+  res.render('index', { introProduct, bestProduct, products });
 });
 
 module.exports = router;
