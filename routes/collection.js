fe68d35bdca44f7e247fdb58a4cf4edf4781476a
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-25 20:49:14 +0500

    connection shoes page

--- routes/collection.js
+++ routes/collection.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { row, rows } = require("../util/pg")
+const { rows } = require("../util/pg")
 
 
 const introSQL = `
--- routes/shoes.js
+++ routes/shoes.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const { row, rows } = require("../util/pg")
 
-router.get('/', function(req, res, next) {
-  res.render('shoes');
+const productsSQL = `
+  select * from products where product_new = false and product_rating = 5
+`
+
+router.get('/', async function(req, res, next) {
+
+  const products = await rows(productsSQL)
+
+  res.render('shoes', {products});
 });
 
 module.exports = router;
