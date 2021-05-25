cf9e0e8f011cf1ea36c4331c9afb261212df16c8
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-25 16:23:58 +0500

    Add connect database

--- util/pg.js
+++ util/pg.js
@@ -1,1 +1,45 @@
+const { cache } = require('ejs')
 const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: 'muhammadbobur',
+  database: 'pollo',
+  password: '1111',
+  port: 5432,
+  host: 'localhost'
+})
+
+const rows = async (SQL, ...params) => {
+
+  const clint = await pool.connect()
+
+  try {
+
+    const { rows } = await clint.query(SQL, params)
+
+    return rows
+  } catch(e) {
+    console.log(e);
+  } finally{
+    clint.release()
+  }
+}
+
+const row = async (SQL, ...params) => {
+
+  const clint = await pool.connect()
+
+  try {
+
+    const { rows: [row] } = await clint.query(SQL, params)
+
+    return row
+  } catch(e) {
+    console.log(e);
+  } finally{
+    clint.release()
+  }
+}
+
+module.exports.row = row
+module.exports.rows = rows
